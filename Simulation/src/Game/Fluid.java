51e99d403aab6b88fc9b50e2e04c6a79a73367c4
flmg <flo.magot@gmail.com>
2013-03-25 23:13:24 +0100

    Basic fluid system (Right click to create)

---
+++ Simulation/src/Game/Fluid.java
@@ -0,0 +1,100 @@
+package Game;
+
+import java.util.Random;
+
+import org.newdawn.slick.*;
+
+public class Fluid {
+
+	private int[][] map;
+	private int w, h;
+	private Color c;
+	public int scale;
+
+	public Fluid(GameContainer gc, Color col, int s) {
+		this.scale = s;
+		w = gc.getWidth() / scale;
+		h = gc.getHeight() / scale;
+		this.c = col;
+		map = new int[w][h];
+		for (int j = 0; j < h; j++) {
+			for (int i = 0; i < w; i++) {
+				map[i][j] = 0;
+			}
+		}
+	}
+
+	public boolean getRandomBoolean() {
+		Random random = new Random();
+		return random.nextBoolean();
+	}
+
+	public void add(int x, int y) {
+		map[x][y] = 1;
+	}
+
+	public void free(int x, int y) {
+		map[x][y] = 0;
+	}
+
+	// return true if success
+	public boolean adddown(int i, int j) {
+		if (j + 1 < h && map[i][j + 1] == 0) {
+			add(i, j + 1);
+			free(i, j);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean addright(int i, int j) {
+		if (i + 1 < w && map[i + 1][j] == 0) {
+			add(i + 1, j);
+			free(i, j);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean addleft(int i, int j) {
+		if (i - 1 >= 0 && map[i - 1][j] == 0) {
+			add(i - 1, j);
+			free(i, j);
+			return true;
+		}
+		return false;
+	}
+
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		for (int j = 0; j < h; j++) {
+			for (int i = 0; i < w; i++) {
+				if (map[i][j] == 1) {
+					g.setColor(c);
+					g.fillRect(i * scale, j * scale, scale, scale);
+					g.flush();
+				}
+			}
+		}
+	}
+
+	public void update(GameContainer gc, double dt) throws SlickException {
+		for (int j = h - 2; j >= 0; j--) {
+			for (int i = 0; i < w; i++) {
+				// if there is fluid
+				if (map[i][j] == 1) {
+					// try to add down first
+					if (!adddown(i, j)) {
+						// 50% chance for it to go right
+						if (getRandomBoolean()) {
+							if (!addright(i, j))
+								addleft(i, j);
+						} else {
+							if (!addleft(i, j))
+								addright(i, j);
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- Simulation/src/Game/Main.java
+++ Simulation/src/Game/Main.java
@@ -1,12 +1,14 @@
 package Game;
 
 import Game.ParticulesHandler;
+import Game.Fluid;
 import org.newdawn.slick.*;
 
 public class Main extends BasicGame {
 
 	private Image texture;
 	private ParticulesHandler particules = new ParticulesHandler();
+	private Fluid water;
 	private boolean old_MouseButtonDown;
 
 	public Main() {
@@ -20,16 +22,22 @@ public class Main extends BasicGame {
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
+		this.water = new Fluid(gc, Color.blue,4);
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
 		Input ip = gc.getInput();
 		particules.update(gc, (double)delta / 1000f);
-
+		water.update(gc, delta);
+		
 		// Mouse left clicked
 		if (ip.isMouseButtonDown(0) && !old_MouseButtonDown) {
-			particules.addtolist(ip.getMouseX(), ip.getMouseY(), 1024, 720, texture);
+			particules.addtolist(ip.getMouseX(), ip.getMouseY(), 1024, 500, texture);
+		}
+		// Mouse right clicked (add sand)
+		if (ip.isMouseButtonDown(1)) {
+			water.add(ip.getMouseX()/water.scale, ip.getMouseY()/water.scale);
 		}
 		old_MouseButtonDown = ip.isMouseButtonDown(0);
 	}
@@ -37,11 +45,12 @@ public class Main extends BasicGame {
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		particules.render(gc, g);
+		water.render(gc, g);
 	}
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new Main());
-		app.setDisplayMode(1024, 720, false);
+		app.setDisplayMode(1024, 500, false);
 		app.start();
 	}
 }
