cb44d547e7495dd63a0db81085c8203edfd91493
aerr <romsahel@gmail.com>
2013-03-28 14:14:23 -0700

    forgotten file

---
+++ Simulation/src/Game/Line.java
@@ -0,0 +1,17 @@
+package Game;
+
+public class Line {
+	
+	public float x0;
+	public float y0;
+	public float x1;
+	public float y1;
+	
+	public Line(float x0, float y0, float x1, float y1)
+	{
+		this.x0 = x0;
+		this.y0 = y0;
+		this.x1 = x1;
+		this.y1 = y1;
+	}
+}
